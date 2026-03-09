e37b262f5eae14e37a5b78c69ae3d3553379e282
root@op.lan <root@op.lan>
2026-03-09 23:05:41 +0800

    init

---
+++ get_ip.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+set -e
+
+cd $(dirname $0)
+curr=$(pwd)
+dest="$curr/ip.txt"
+key="$curr/get_ip.password.tmp"
+
+encode() {
+    cd ..
+    git pull
+
+    cd $curr
+    theip=$(ip addr | grep "inet6* " | sort)
+
+    echo -n "$theip" | openssl enc -aes-256-cbc -salt -base64 -k "$(cat $key)" > $dest
+
+    git add $dest
+    git commit -m "update ip"
+    git push
+}
+
+decode() {
+    cat $dest | openssl enc -d -aes-256-cbc -base64 -k "$(cat $key)"
+}
+
+if [ "$1" = "decode" ]; then
+    decode
+else
+    s=$(uname -a | grep OpenWrt) || true
+    if [ -n "$s" ]; then
+        encode
+    else
+        echo "Only OpenWrt system can run this script to update ip address, but you can run 'sh get_ip.sh decode' to get the ip address if you have the password."
+    fi
+fi
